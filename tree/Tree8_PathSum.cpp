b29f9fc835bcf7408d740ad1a7021640015d14a9
Smriti Mishra <hi.smritimishra@gmail.com>
2022-04-26 18:43:00 -0300

    Data Structure I Question

---
+++ tree/Tree8_PathSum.cpp
@@ -0,0 +1,18 @@
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        
+        if(root == NULL){
+            return false;
+        }
+        
+        else if(root->val == targetSum && root->left == NULL && root->right == NULL){
+            return true;
+        }
+        
+        else{
+            int newTarget = targetSum - root->val;
+            return hasPathSum(root->left, newTarget) || 
+                   hasPathSum(root->right, newTarget);
+            
+        }
+        
+    }
