85aaaf49f688754640995513eff9da50100ae3df
Smriti Mishra <hi.smritimishra@gmail.com>
2022-04-26 18:15:56 -0300

    Data Structure I Question

---
+++ tree/Tree7_Invert_BinaryTree.cpp
@@ -0,0 +1,9 @@
+   TreeNode* invertTree(TreeNode* root) {
+        if(root == NULL)
+            return root;
+        
+        swap(root->left, root->right);
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
