a89c652f7bfee40342e4fa138ad0b917769f78e0
Smriti Mishra <hi.smritimishra@gmail.com>
2022-04-29 14:44:53 -0300

    Data Structure II Quuestion

---
+++ array/Array17_RotateImage.cpp
@@ -0,0 +1,19 @@
+ void rotate(vector<vector<int>>& matrix) {
+        int row = matrix[0].size();
+        int column = matrix.size();
+        
+       
+        
+        //Transpose
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < i; j++){
+                swap(matrix[i][j] , matrix[j][i]); 
+            }
+        }
+        
+        //Reverse
+        for(int i = 0; i < row; i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+        
+    }
